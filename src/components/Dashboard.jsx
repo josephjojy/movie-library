bf51079b1218854de32aebc12c64a862988ffc6a
Joseph Jojy <joseph.jojy61@gmail.com>
2022-01-15 22:40:31 +0530

    Added Movies to feed

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
+import { omdbapi } from './apis/omdb'
+import Feed from './Feed'
 import Header from './Header'
 import Search from './Search'
 
 const Dashboard = () => {
+    const [page,setPage] = useState(1);
+    const [searchText,setSearchText] = useState("Friends");
+    const [movieResult, setMovieResult] = useState();
+    const [loading, setloading] = useState(true);
+
+    const fetchMovies = async() =>{
+        try{
+            const response = await omdbapi({
+                s: searchText,
+                page
+            });
+            setMovieResult(response.data);
+            setloading(false)
+        }catch(error){console.log(error)}
+    }
+
+    useEffect(() => {
+        fetchMovies();
+    }, [page,searchText])
+
     return (
-        <div className='flex flex-col w-screen min-h-screen text-white bg-slate-700'>
+        <div className='flex flex-col min-h-screen text-white bg-slate-700'>
             <Header />
             <Search />
+            <Feed movieResult={movieResult}  loading={loading}/>
         </div>
     )
 }
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Card from './Card'
 
-const Feed = () => {
+import { PageLoader } from '@bigbinary/neetoui/v2'
+const Feed = ({ movieResult, loading }) => {
+
+    if (loading)
+        return (<div className=' min-h-screen flex justify-center items-center'><PageLoader /></div>)
     return (
         <div>
-            
+            <div className='flex flex-wrap justify-center mt-4'>
+                {movieResult?.Search.map((movie, index) => (
+                    <Card key={index} movie={movie} />
+                ))}
+            </div>
         </div>
     )
 }
