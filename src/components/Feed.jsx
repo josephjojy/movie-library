21df52372de61c7900837f876df8481cbc9f4b9b
Joseph Jojy <joseph.jojy61@gmail.com>
2022-01-15 23:44:40 +0530

    Added Pagination

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Card from './Card'
+import { Pagination, PageLoader } from "@bigbinary/neetoui/v2";
 
-import { PageLoader } from '@bigbinary/neetoui/v2'
-const Feed = ({ movieResult, loading }) => {
+const Feed = ({ movieResult, loading, page, setPage }) => {
 
     if (loading)
         return (<div className=' min-h-screen flex justify-center items-center'><PageLoader /></div>)
@@ -12,6 +12,14 @@ const Feed = ({ movieResult, loading }) => {
                 {movieResult?.Search.map((movie, index) => (
                     <Card key={index} movie={movie} />
                 ))}
+            </div>
+            <div className='flex justify-center pb-8'>
+                <Pagination
+                    count={movieResult.totalResults}
+                    navigate={(e)=>{setPage(e)}}
+                    pageNo={page}
+                    pageSize={10}
+                />
             </div>
         </div>
     )
