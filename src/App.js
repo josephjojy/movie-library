58045b95817b242a97af24ea2f67c79171006c07
Joseph Jojy <joseph.jojy61@gmail.com>
2022-01-15 21:04:05 +0530

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import {Route, BrowserRouter, Switch} from 'react-router-dom'
+
+import Dashboard from './components/Dashboard'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path='/' component={Dashboard} />
+        </Switch>
+      </BrowserRouter>
+    </>
   )
 }
 
