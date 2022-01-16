57a8d6ae63026208fcfd1361379bbe1f1c883715
Joseph Jojy <joseph.jojy61@gmail.com>
2022-01-16 16:33:23 +0530

    Movie Search results

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,60 @@
-import React from 'react'
-
-import { Input } from "@bigbinary/neetoui/v2"
+import React, { useState, useCallback } from 'react'
+import { omdbapi } from '../apis/omdb'
+import { Input, Button } from "@bigbinary/neetoui/v2"
 import { Search as SearchIcon, Close as CloseIcon } from "@bigbinary/neeto-icons"
+import debounce from "lodash/debounce"
 
-const Search = () => {
+const Search = ({ setSearchMovie }) => {
+    const [searchText, setSearchText] = useState("");
+    const [searchResult, setSearchResult] = useState();
+
+
+    const clearData = () => {
+        setSearchText("");
+        setSearchResult();
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        searchText.length ? setSearchMovie(searchText) : setSearchMovie("Friends")
+        setSearchResult();
+    }
+
+    const fetchMovies = async (searchText) => {
+        try {
+            const response = await omdbapi({
+                s: searchText,
+                page: 1
+            });
+            setSearchResult(response.data);
+        } catch (error) { console.log(error) }
+    }
+
+    const searchDebounce = useCallback(debounce(value => fetchMovies(value), 500), [])
 
     return (
         <div className=" mt-4 flex flex-col items-center justify-center">
-            <Input className = "w-1/3" label={<span className='text-white'>Search for your favorite movies</span>} prefix={<SearchIcon />} suffix ={<CloseIcon />}/>
+            <form className="searchBox" onSubmit={(event) => handleSubmit(event)}>
+                <Input
+                    className="w-full"
+                    placeholder="Search for your favorite movies"
+                    prefix={<SearchIcon />}
+                    suffix={searchText && <Button icon={CloseIcon} style="text" onClick={() => clearData()} />}
+                    value={searchText}
+                    onChange={(e) => { setSearchText(e.target.value); searchDebounce(e.target.value) }}
+                    onBlur={() => clearData()}
+                />
+            </form>
+            {searchResult && <div className="searchBox absolute top-32 mt-2 max-h-72 overflow-y-auto bg-white rounded-sm border-b-2 z-50">
+                {
+                    searchResult?.Search?.map((ele, index) => {
+                        return (
+                            <div className="m-2 p-2 bg-gray-100 rounded-sm border-b-2 text-black" key={index}>
+                                {ele.Title}
+                            </div>)
+                    })
+                }
+            </div>}
         </div>
     )
 }
